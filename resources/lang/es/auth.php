253a9c45f0c9ac3bc69e70af53b9ff1086786fb2
easley1290 <easley.gc@gmail.com>
2021-02-18 21:04:45 -0400

    Primer commit

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Esas credenciales no coinciden con nuestros registros.',
+    'password' => 'La contraseña brindada no es correcta.',
+    'throttle' => 'Demasiados intentos de login fallidos. Por favor intenta de nuevo en :seconds segundos.',
+
+];
---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Tu contraseña ha sido restaurada.',
+    'sent' => 'Te enviamos el link de restauracion de contraseña a tu correo.',
+    'throttled' => 'Por favor espere antes de reintentar',
+    'token' => 'Este token de restauracion es invalido',
+    'user' => "No pudimos encontrar un usuario con este correo.",
+
+];
