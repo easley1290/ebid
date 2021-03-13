cefe54c1196510a0acbac0babb6c0ffc03272e85
alejandro quehui <alejandroqm832@gmail.com>
2021-03-13 16:09:12 -0400

    creación de pensums por especialidad

--- app/Http/Controllers/PensumController.php
+++ app/Http/Controllers/PensumController.php
@@ -19,15 +19,100 @@ class PensumController extends Controller
      */
     public function index()
     {
-        $pensum = Pensum::all();
-        $carreras = Carreras::all();
-        $semestre = Semestre::all();
-        $materias = Materias::all();
-        $estados = Subdominios::select('subdominios.*')
-        ->where('subd_dom_id','=',1)
-        ->get();
-        $aux = [$pensum, $estados, $carreras, $materias, $semestre];
-        return view('ebid-views-administrador.pensum.pensum')->with('aux', $aux);
+        try{
+            $pensum = Pensum::all();
+            $carreras = Carreras::all();
+            $semestre = Semestre::all();
+            $materias = Materias::all();
+            $estados = Subdominios::select('subdominios.*')
+            ->where('subd_dom_id','=',1)
+            ->get();
+            $pensum_cd_1 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-CD-01")
+            ->where('pen_sem_id','=',1)
+            ->get();
+            $pensum_cd_2 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-CD-01")
+            ->where('pen_sem_id','=',2)
+            ->get();
+            $pensum_cd_3 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-CD-01")
+            ->where('pen_sem_id','=',3)
+            ->get();
+            $pensum_cd_4 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-CD-01")
+            ->where('pen_sem_id','=',4)
+            ->get();
+            $pensum_dc_1 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-DC-01")
+            ->where('pen_sem_id','=',1)
+            ->get();
+            $pensum_dc_2 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-DC-01")
+            ->where('pen_sem_id','=',2)
+            ->get();
+            $pensum_dc_3 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-DC-01")
+            ->where('pen_sem_id','=',3)
+            ->get();
+            $pensum_dc_4 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-DC-01")
+            ->where('pen_sem_id','=',4)
+            ->get();
+            $pensum_df_1 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-DF-01")
+            ->where('pen_sem_id','=',1)
+            ->get();
+            $pensum_df_2 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-DF-01")
+            ->where('pen_sem_id','=',2)
+            ->get();
+            $pensum_df_3 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-DF-01")
+            ->where('pen_sem_id','=',3)
+            ->get();
+            $pensum_df_4 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-DF-01")
+            ->where('pen_sem_id','=',4)
+            ->get();
+            $pensum_dmc_1 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-DMC-01")
+            ->where('pen_sem_id','=',1)
+            ->get();
+            $pensum_dmc_2 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-DMC-01")
+            ->where('pen_sem_id','=',2)
+            ->get();
+            $pensum_dmc_3 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-DMC-01")
+            ->where('pen_sem_id','=',3)
+            ->get();
+            $pensum_dmc_4 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-DMC-01")
+            ->where('pen_sem_id','=',4)
+            ->get();
+            $pensum_pd_1 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-PD-01")
+            ->where('pen_sem_id','=',1)
+            ->get();
+            $pensum_pd_2 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-PD-01")
+            ->where('pen_sem_id','=',2)
+            ->get();
+            $pensum_pd_3 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-PD-01")
+            ->where('pen_sem_id','=',3)
+            ->get();
+            $pensum_pd_4 = Pensum::select('pensum.*')
+            ->where('pen_car_id','=',"E-PD-01")
+            ->where('pen_sem_id','=',4)
+            ->get();
+            $aux = [$pensum, $estados, $carreras, $materias, $semestre, $pensum_cd_1, $pensum_cd_2, $pensum_cd_3, $pensum_cd_4, $pensum_dc_1, $pensum_dc_2, $pensum_dc_3, $pensum_dc_4, $pensum_df_1, $pensum_df_2, $pensum_df_3, $pensum_df_4, $pensum_dmc_1, $pensum_dmc_2, $pensum_dmc_3, $pensum_dmc_4, $pensum_pd_1, $pensum_pd_2, $pensum_pd_3, $pensum_pd_4];
+            return view('ebid-views-administrador.pensum.pensum')->with('aux', $aux);
+        } 
+        catch (Throwable $e){
+            return view('ebid-views-administrador.home')->with('status', 'Hubo un error inusual');
+        }
     }
 
     /**
@@ -48,19 +133,24 @@ class PensumController extends Controller
      */
     public function store(Request $request)
     {
-        $this->validate($request,[
-            'pen_car_id' => 'required',
-            'pen_mat_id' => 'required',
-            'pen_sem_id' => 'required',
-            'pen_subd_estado' => 'required',
-        ]);
-        $pensum_nueva = new Pensum;
-        $pensum_nueva->pen_car_id = $request->input('pen_car_id');
-        $pensum_nueva->pen_mat_id = $request->input('pen_mat_id');
-        $pensum_nueva->pen_sem_id = $request->input('pen_sem_id');
-        $pensum_nueva->pen_subd_estado = $request->input('pen_subd_estado');
-        $pensum_nueva->save();
-        return redirect('/Pensum')->with('success', 'Dato guardado');
+        try{
+            $this->validate($request,[
+                'pen_car_id' => 'required',
+                'pen_mat_id' => 'required',
+                'pen_sem_id' => 'required',
+                'pen_subd_estado' => 'required',
+            ]);
+            $pensum_nueva = new Pensum;
+            $pensum_nueva->pen_car_id = $request->input('pen_car_id');
+            $pensum_nueva->pen_mat_id = $request->input('pen_mat_id');
+            $pensum_nueva->pen_sem_id = $request->input('pen_sem_id');
+            $pensum_nueva->pen_subd_estado = $request->input('pen_subd_estado');
+            $pensum_nueva->save();
+            return redirect()->route('Pensum.index')->with('status', 'Se AGREGÓ una materia al pensum con exito');
+        } 
+        catch (Throwable $e){
+            return view('ebid-views-administrador.home')->with('status', 'Hubo un error inusual');
+        }
     }
 
     /**
@@ -94,20 +184,24 @@ class PensumController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $this->validate($request,[
-            'pen_car_id' => 'required',
-            'pen_mat_id' => 'required',
-            'pen_sem_id' => 'required',
-            'pen_subd_estado' => 'required',
-        ]);
-        $pensum_edit = Pensum::find($id);
-        $pensum_edit->pen_car_id = $request->input('pen_car_id');
-        $pensum_edit->pen_mat_id = $request->input('pen_mat_id');
-        $pensum_edit->pen_sem_id = $request->input('pen_sem_id');
-        $pensum_edit->pen_subd_estado = $request->input('pen_subd_estado');
-        $pensum_edit->save();
-        
-        return redirect('/Pensum')->with('success', 'Dato guardado');
+        try{
+            $this->validate($request,[
+                'pen_car_id' => 'required',
+                'pen_mat_id' => 'required',
+                'pen_sem_id' => 'required',
+                'pen_subd_estado' => 'required',
+            ]);
+            $pensum_edit = Pensum::find($id);
+            $pensum_edit->pen_car_id = $request->input('pen_car_id');
+            $pensum_edit->pen_mat_id = $request->input('pen_mat_id');
+            $pensum_edit->pen_sem_id = $request->input('pen_sem_id');
+            $pensum_edit->pen_subd_estado = $request->input('pen_subd_estado');
+            $pensum_edit->save();
+            return redirect()->route('Pensum.index')->with('status', 'Se MODIFICÓ una materia del pensum con exito');
+        } 
+        catch (Throwable $e){
+            return view('ebid-views-administrador.home')->with('status', 'Hubo un error inusual');
+        }
     }
 
     /**
@@ -118,8 +212,13 @@ class PensumController extends Controller
      */
     public function destroy($id)
     {
-        $pensum_delete = Pensum::find($id);
-        $pensum_delete->delete();
-        return redirect('/Pensum')->with('success', 'Dato guardado');
+        try{   
+            $pensum_delete = Pensum::find($id);
+            $pensum_delete->delete();
+            return redirect()->route('Pensum.index')->with('status', 'Se ELIMINÓ una materia del pensum con exito');
+        } 
+        catch (Throwable $e){
+            return view('ebid-views-administrador.home')->with('status', 'Hubo un error inusual');
+        }
     }
 }
--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -54,7 +54,7 @@ class PersonaController extends Controller
     public function store(Request $request)
     {
         try{
-            $this->validate($request,[
+                $this->validate($request,[
                 'nombres' => 'required',
                 'paterno' => 'required',
                 'materno' => 'required',
@@ -64,48 +64,27 @@ class PersonaController extends Controller
                 'extension' => 'required',
                 'fec_nac' => 'required',
                 'correo' => 'required'
-            ]);
-            
-            $persona_nuevo = new Personas;
-
-<<<<<<< HEAD
-            $persona_nuevo->per_ua_id =             'UA-EA0001';
-            $persona_nuevo->per_nombres =           $request->nombres;
-            $persona_nuevo->per_paterno =           $request->paterno;
-            $persona_nuevo->per_materno =           $request->materno;
-            $persona_nuevo->per_num_documentacion = $request->num_doc;
-            $persona_nuevo->per_fecha_nacimiento =  $request->fec_nac;
-            $persona_nuevo->per_telefono =          $request->telefono;
-            $persona_nuevo->per_correo_personal =   $request->correo;
-            $persona_nuevo->per_domicilio =         $request->domicilio;
-            $persona_nuevo->per_subd_documentacion =$request->tipo_doc;
-            $persona_nuevo->per_subd_extension =    $request->extension;
-            $persona_nuevo->per_subd_genero =       $request->genero;
-            $persona_nuevo->per_subd_estado =       '1';
-            $persona_nuevo->save();
-            return redirect()->route('Persona.index')->with('status', 'Se AGREGÓ una persona con exito');
-        } 
-        catch (Throwable $e){
-            return view('ebid-views-administrador.home')->with('status', 'Hubo un error inusual');
-        }
-=======
-        $persona_nuevo->per_ua_id =             'UA-EA0001';
-        $persona_nuevo->per_nombres =           $request->nombres;
-        $persona_nuevo->per_paterno =           $request->paterno;
-        $persona_nuevo->per_materno =           $request->materno;
-        $persona_nuevo->per_num_documentacion = $request->num_doc;
-        $persona_nuevo->per_fecha_nacimiento =  $request->fec_nac;
-        $persona_nuevo->per_telefono =          $request->telefono;
-        $persona_nuevo->per_correo_personal =   $request->correo;
-        $persona_nuevo->per_domicilio =         $request->domicilio;
-        $persona_nuevo->per_subd_documentacion =$request->tipo_doc;
-        $persona_nuevo->per_subd_extension =    $request->extension;
-        $persona_nuevo->per_subd_genero =       $request->genero;
-        $persona_nuevo->per_subd_estado =       '1';
-        $persona_nuevo->save();
-
-        return redirect()->route('Persona.index');
->>>>>>> d6c885a892256c9b2f743394341bbd8f19c6275f
+                ]);
+                $persona_nuevo = new Personas;
+                $persona_nuevo->per_ua_id =             'UA-EA0001';
+                $persona_nuevo->per_nombres =           $request->nombres;
+                $persona_nuevo->per_paterno =           $request->paterno;
+                $persona_nuevo->per_materno =           $request->materno;
+                $persona_nuevo->per_num_documentacion = $request->num_doc;
+                $persona_nuevo->per_fecha_nacimiento =  $request->fec_nac;
+                $persona_nuevo->per_telefono =          $request->telefono;
+                $persona_nuevo->per_correo_personal =   $request->correo;
+                $persona_nuevo->per_domicilio =         $request->domicilio;
+                $persona_nuevo->per_subd_documentacion =$request->tipo_doc;
+                $persona_nuevo->per_subd_extension =    $request->extension;
+                $persona_nuevo->per_subd_genero =       $request->genero;
+                $persona_nuevo->per_subd_estado =       '1';
+                $persona_nuevo->save();
+                return redirect()->route('Persona.index');
+            }
+            catch (Throwable $e){
+                return view('ebid-views-administrador.home')->with('status', 'Hubo un error inusual');
+            }
     }
 
     /**
@@ -156,29 +135,12 @@ class PersonaController extends Controller
 
             $persona_edit = Personas::find($id);
 
-<<<<<<< HEAD
-            $persona_edit->per_nombres = $request->input('per_nombres');
-            $persona_edit->per_paterno = $request->input('per_paterno');
-            $persona_edit->per_materno = $request->input('per_materno');
-            $persona_edit->per_num_documentacion = $request->input('per_num_documentacion');
-            $persona_edit->per_fecha_nacimiento = $request->input('per_fecha_nacimiento');
-            $persona_edit->per_telefono = $request->input('per_telefono');
-            $persona_edit->per_correo_personal = $request->input('per_correo_personal');
-            $persona_edit->per_domicilio = $request->input('per_domicilio');
-            $persona_edit->per_subd_documentacion = $request->input('per_subd_documentacion');
-            $persona_edit->per_subd_extension = $request->input('per_subd_extension');
-            $persona_edit->per_subd_genero = $request->input('per_subd_genero');
-            $persona_edit->per_subd_estado = '1';
             $persona_edit->save();
-            return redirect()->route('Persona.index')->with('status', 'Se MODIFICÓ una persona con exito');
-        } 
+            return redirect()->route('Persona.index');
+        }
         catch (Throwable $e){
             return view('ebid-views-administrador.home')->with('status', 'Hubo un error inusual');
         }
-=======
-        $persona_edit->save();
-        return redirect()->route('Persona.index');
->>>>>>> d6c885a892256c9b2f743394341bbd8f19c6275f
     }
 
     /**
@@ -189,20 +151,9 @@ class PersonaController extends Controller
      */
     public function destroy($id)
     {
-<<<<<<< HEAD
-        try{
-            $persona_delete = Personas::find($id);
-            $persona_delete->delete();
-            return redirect()->route('Persona.index')->with('status', 'Se ELIMINÓ una persona con exito');
-        } 
-        catch (Throwable $e){
-            return view('ebid-views-administrador.home')->with('status', 'Hubo un error inusual');
-        }
-=======
         $persona_delete = Personas::find($id);
 
         $persona_delete->delete();
         return redirect()->route('Persona.index');
->>>>>>> d6c885a892256c9b2f743394341bbd8f19c6275f
     }
 }
